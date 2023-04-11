fc777a3acdb63a02dc7ff36e6c424cdf98ad1cac
Lauren Cody <laurenvcody@gmail.com>
2023-04-11 15:10:02 -0400

    filter

--- prevalence_1.R
+++ prevalence_1.R
@@ -1,4 +1,5 @@
-flower_meta=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_metadata.csv")
+flower_meta=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_metadata.csv")%>%
+  select(-c("...25","...26","...27","...28","...29","...30"))
 flower_qpcr=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_qPCR_master.csv")
 library(dplyr)
 library(tidyverse)
