684b8a5bc2368f93810576ded9b4fe5f9d23cf6d
Lauren Cody <laurenvcody@gmail.com>
2023-04-11 16:50:47 -0400

    ladnscape

--- prevalence_1.R
+++ prevalence_1.R
@@ -9,12 +9,23 @@ varroa <- read_csv("C:/Users/laure/Desktop/Laurens_thesis/varroa-loads.csv") %>%
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
+
 flower_meta_comb=flower_qpcr%>%
   left_join(flower_meta,by=('Sample_ID'))%>%
   mutate(dwv=ifelse(c(SQ>0 & `Melt Temp`<=83 & `Melt Temp`>=81),1,0))%>%
   relocate(dwv,.after=`Melt Temp`)
-prevalence = table(flower_meta_comb$Apiary_ID, flower_meta_comb$dwv)
-prevalence_table = prop.table(prevalence, margin=1)
-prevalence_table
 
-read.csv(aggregate_values)
+prevalence = table(flower_meta_comb$Apiary_ID, flower_meta_comb$dwv)
+prevalence_table = as.data.frame(prop.table(prevalence, margin=1))%>%
+  filter(Var2==1)%>%
+  rename(Apiary_ID=Var1, dwv_presence=Var2)
+
+prevalence_table_2=prevalence_table%>%
+  left_join(varroa)
+
+plot(prevalence_table_2$Freq~prevalence_table_2$varroa_avg)
+
+agg_values=read.csv("C:/Users/laure/Desktop/Laurens_thesis/aggregate_values.csv")%>%
+  filter(Prelim_Agg_Land_Class=='Natural')
+prevalence_table_3=prevalence_table_2%>%
+  left_join(agg_values)
