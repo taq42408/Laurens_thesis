9b8e2cf1c04dfc7422ec299e94d9e9fdd8c2cc7e
Lauren Cody <laurenvcody@gmail.com>
2023-04-12 13:05:21 -0400

    h

--- prevalence_1.R
+++ prevalence_1.R
@@ -1,5 +1,4 @@
-flower_meta=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_metadata.csv")%>%
-  select(-c("...25","...26","...27","...28","...29","...30"))
+flower_meta=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_metadata.csv")
 flower_qpcr=read_csv("C:/Users/laure/Desktop/Laurens_thesis/Flower_qPCR_master.csv")%>%
   filter(Content=="Unkn" & !Sample_ID=="NA")
 varroa <- read_csv("C:/Users/laure/Desktop/Laurens_thesis/varroa-loads.csv") %>% 
